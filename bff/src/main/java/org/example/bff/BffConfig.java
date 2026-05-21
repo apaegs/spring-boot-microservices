2ae7fb9a86b0c75338490def8ecffba0604f9efb
Andreas Kaiberger <andreas.kaiberger@gmail.com>
2026-05-21 15:23:41 +0200

    Configure OAuth2 client and security settings in `bff` project with `BffConfig` class and update application properties

---
+++ bff/src/main/java/org/example/bff/BffConfig.java
@@ -0,0 +1,30 @@
+package se.example.bff;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
+import static org.springframework.cloud.gateway.server.mvc.filter.TokenRelayFilterFunctions.tokenRelay;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
+
+@Configuration
+public class BffConfig {
+
+    @Bean
+    SecurityFilterChain security(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .csrf(csrf -> csrf.disable())
+                .oauth2Login(Customizer.withDefaults())
+                .oauth2Client(Customizer.withDefaults())
+                .build();
+    }
+}
